2c5fcec8e39994cb515984adc5ed1105b7e29c91
Zack Jones <justzackj@gmail.com>
2025-12-27 09:41:44 -0700

    Add seeding from Livetime data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,63 @@
 import { PrismaClient } from '@prisma/client'
+import SeedData from './seed.data.json'
 
 const prisma = new PrismaClient()
 
 async function main() {
-  //Define / edit schedule types as needed
-  const scheduleTypes = [
-    { id: 1, name: 'Saturday Carpet Oval', description: 'Saturday Raceday on the carpet track!' },
-    { id: 2, name: 'Tuesday Carpet Oval', description: 'Tuesday Raceday on the carpet track!' },
-    { id: 3, name: 'Saturday Dirt Oval', description: 'Saturday Raceday on the dirt track!' },
-    { id: 4, name: 'Tuesday Dirt Oval', description: 'Tuesday Raceday on the dirt track!' }
-  ]
+  console.log('Starting seeding process...')
 
-  console.log('Seeding schedule types...')
-  
-  for (const scheduleType of scheduleTypes) {
-    await prisma.scheduleType.upsert({
-      where: { id: scheduleType.id },
-      update: {
-        name: scheduleType.name,
-        description: scheduleType.description,
-      },
-      create: scheduleType,
-    })
-  }
+  //Be careful with this - it will delete existing data.
+  await clearDatabase();
+
+  await seedLiveTimeEvent(SeedData.LiveTimeEvent)
+  await seedEvents(SeedData.TrackEvents)
 
   console.log('Seeding completed!')
+}
+
+async function clearDatabase() {
+  console.log('Clearing database...')
+  await prisma.trackEvent.deleteMany({})
+  await prisma.liveTimeEvent.deleteMany({})
+  console.log('Database cleared.')
+}
+
+async function seedLiveTimeEvent(liveTimeEvents: any[]) {
+  console.log('Seeding LiveTimeEvent...')
+
+  for (const liveTimeEvent of liveTimeEvents) {
+    await prisma.liveTimeEvent.upsert({
+      where: { id: liveTimeEvent.id },
+      update: { 
+        name: liveTimeEvent.name, startedAt: liveTimeEvent.startedAt,
+        entries: liveTimeEvent.entries, drivers: liveTimeEvent.drivers },
+      create: { ...liveTimeEvent },
+    })
+    console.log(`Seeded LiveTimeEvent: ${liveTimeEvent.name} (id=${liveTimeEvent.id})`)
+  }
+  console.log(`Seeded ${liveTimeEvents.length} LiveTimeEvents.`)
+}
+
+//Seeding events is more important for initial data & development
+//  Be careful when running in production as it will overwrite existing data
+async function seedEvents(events: any[]) {
+  console.log('Seeding Events...')
+  
+  for (const event of events) {
+    await prisma.trackEvent.upsert({
+      where: { livetimeID: event.livetimeID },
+      update: {
+        name: event.name
+      }, 
+      create: {
+        ...event,
+        start: new Date(event.start),
+        end: new Date(event.end),
+      }
+    })
+    console.log(`Seeded TrackEvent: ${event.name} (id=${event.livetimeID})`)
+  }
+  console.log(`Seeded ${events.length} TrackEvents.`)
 }
 
 main()
---
+++ tools/livetime_scraper/livetime.rc.scraper.py
@@ -0,0 +1,125 @@
+import bs4, requests, json
+from datetime import datetime
+
+
+class EventBrief():
+    def __init__(self, row: bs4.element.Tag):
+        cols = row.find_all("td")
+        first = cols[0]
+        
+        # The ID is in a url like this: /results/?p=view_event&id=488422
+        self.event_id = int(first.find("a")["href"].split("&id=")[-1])
+        self.name = first.text.strip()
+
+        # the date is formatted like: <span class="hidden">2025-12-02 00:00:00</span>Dec 2, 2025
+        date_str = cols[1].find("span").text.strip()
+        # Convert to ISO 8601 format: "2024-07-15T18:00:00.000Z"
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        self.date = date_obj.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+        # These values are just numeric in the table
+        self.entries = int(cols[2].text.strip())
+        self.drivers = int(cols[3].text.strip())
+
+        self.livetime_path = f"/results/?p=view_event&id={self.event_id}"
+
+        self.laps = 0
+
+    def add_statistics(self, laps: int):
+        self.laps = laps
+    
+    def __str__(self):
+        return f"EventBrief(event_id={self.event_id}, name={self.name}, date={self.date}, entries={self.entries}, drivers={self.drivers})"
+
+
+# The root URL for our livetime instance
+LIVETIME_ROOT_URL = "https://jjsraceway.liverc.com/"
+def get_livetime_url(path: str) -> str: return LIVETIME_ROOT_URL + path.lstrip("/")
+
+def scrape_url(url: str) -> bs4.BeautifulSoup:
+    response = requests.get(url)
+    soup = None
+    if response.status_code == 200:
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+    else:
+        raise Exception(f"Failed to retrieve content from {url}, status code: {response.status_code}")
+    return soup
+
+def scrape_livetime_page(path: str) -> bs4.BeautifulSoup:
+    url = get_livetime_url(path)
+    return scrape_url(url)
+
+def extract_events() -> list[EventBrief]:
+    soup = scrape_livetime_page("/events/")
+
+    events = []
+    # The events table on /events has ID #events 
+    # and is loaded all at once
+    events_table = soup.find("table", {"id": "events"})
+    event_rows = events_table.find("tbody").find_all("tr")
+    for row in event_rows:
+        cols = row.find_all("td")
+        first = cols[0]
+        
+        # The ID is in a url like this: /results/?p=view_event&id=488422
+        id = first.find("a")["href"].split("&id=")[-1]
+        name = first.text.strip()
+
+        # the date is formatted like: <span class="hidden">2025-12-02 00:00:00</span>Dec 2, 2025
+        date = cols[1].find("span").text.strip()
+
+        # These values are just numeric in the table
+        entries = int(cols[2].text.strip())
+        drivers = int(cols[3].text.strip())
+
+        event_brief = EventBrief(row)
+        events.append(event_brief)
+    events.reverse()  # Reverse to have oldest events first
+    return events
+
+def export_events_to_json(events: list[EventBrief], filepath: str):
+    eventsData = []
+    livetimeData = []
+    for event in events:
+        eventsData.append({
+            "start": event.date,
+            "end": event.date,
+            "name": event.name,
+            "visible": True,
+            "cancelled": False,
+            "livetimeID": event.event_id,
+            "updatedAt": event.date,
+            "createdAt": event.date
+        })
+
+        livetimeData.append({
+            "id": event.event_id,
+            "name": event.name,
+            "entries": event.entries,
+            "drivers": event.drivers,
+            "laps": event.laps,
+            "startedAt": event.date,
+            "createdAt": event.date,
+            "updatedAt": event.date
+        })
+
+    totalData = {
+        "TrackEvents": eventsData,
+        "LiveTimeEvent": livetimeData
+    }
+    with open(filepath, 'w') as f: json.dump(totalData, f, indent=4)
+
+if __name__ == "__main__":
+    events = extract_events()
+    for event in events: print(event)
+    export_events_to_json(events, "seed.data.json")
+
+
+
+
+
+
+
+
+
+
