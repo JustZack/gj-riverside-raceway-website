d82d7085a704ef617bd072e173192d506425e4f5
Zack Jones <justzackj@gmail.com>
2025-12-26 18:18:19 -0700

    Update build-and-run-prod.sh

--- build-and-run-prod.sh
+++ build-and-run-prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git pull origin main
 npm install --production
+npm run prisma:generate
 npm run build
-pm2 restart gj-raceway
+pm2 delete gj-raceway
+pm2 start npm --name "gj-raceway" -- start
