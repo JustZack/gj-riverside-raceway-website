f07bf03f965b802909fa1d2f522144db947ce1bf
Zack Jones <justzackj@gmail.com>
2025-12-26 18:36:13 -0700

    use CDN for font awesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export const metadata: Metadata = {
   title: "GJ Riverside Raceway",
@@ -14,6 +13,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+            <link rel="stylesheet" 
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+                integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+                crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
       <body>{children}</body>
     </html>
   );
