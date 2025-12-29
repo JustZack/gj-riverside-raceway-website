d99084a9eeb29962275152df0e8e5324e703957f
Zack Jones <justzackj@gmail.com>
2025-12-29 13:20:17 -0700

    Consolidate components/ui/ imports

---
+++ components/ui/ui.tsx
@@ -0,0 +1,7 @@
+import Row from '@/components/ui/row'
+import Column from '@/components/ui/column'
+import Card from '@/components/ui/card'
+import Banner from '@/components/ui/banner'
+import Button from '@/components/ui/button'
+
+export { Row, Column, Card, Banner, Button }
