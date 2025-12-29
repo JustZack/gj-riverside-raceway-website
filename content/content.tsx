332c6a7d56a255885d15734dcd9d7b9f00b6ff58
Zack Jones <justzackj@gmail.com>
2025-12-29 13:42:36 -0700

    Update content.tsx

--- content/content.tsx
+++ content/content.tsx
@@ -3,6 +3,8 @@ import { footer } from '@/content/footer'
 import { socials } from '@/content/socials'
 import { navigation } from '@/content/navigation'
 import { rules } from '@/content/rules'
+import { facebook } from '@/content/facebook'
+import { youtube } from '@/content/youtube'
 import { livetime } from '@/content/livetime'
 
-export { header, footer, socials, navigation, rules, livetime }
+export { header, footer, socials, navigation, rules, facebook, youtube, livetime }
