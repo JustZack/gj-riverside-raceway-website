93fe4c8c49a897dae6161cf9b2fb1b1ab7fbb9fe
Zack Jones <justzackj@gmail.com>
2025-12-31 09:56:31 -0700

    Update brief.content.row.tsx

--- components/site/brief/brief.content.row.tsx
+++ components/site/brief/brief.content.row.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { InfoWithSubtext, Row, Chip } from '@/components/ui/ui'
+import { InfoWithSubtext, ContentWithIcon, Row, Chip } from '@/components/ui/ui'
 
 type BriefContentRowProps = {
     icon: string,
@@ -12,13 +12,12 @@ type BriefContentRowProps = {
 export default function BriefContentRow({icon, chipClass, name, mainText, subtext = ""}: BriefContentRowProps) {
     function renderBriefContentRow(icon: string, chipClass: string, name: string, mainText: string, subtext: string = "") {
         return (
-            <Row className="flex items-center justify-between gap-2 w-full" gap={1}>
-                <span className="flex items-center justify-center w-6 h-6"><i className={icon}/></span>
+            <ContentWithIcon icon={icon}>
                 <InfoWithSubtext className="flex flex-col min-w-0" style={{width: 'auto'}} subText={subtext}>
                     <Chip className={chipClass} width="95px">{name}</Chip>
                     <span className="font-semibold truncate">{mainText}</span>
                 </InfoWithSubtext>
-            </Row>
+            </ContentWithIcon>
         )
     }
 
