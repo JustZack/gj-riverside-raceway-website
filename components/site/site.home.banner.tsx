72c400b4978b183bf0405b7d29f9e8b2c0c0f2a2
Zack Jones <justzackj@gmail.com>
2026-01-24 09:55:38 -0700

    Add new home page banner

--- components/site/site.home.banner.tsx
+++ components/site/site.home.banner.tsx
@@ -11,17 +11,75 @@ export default function SiteHomeBanner() {
     }
 
     function getRandomBannerImage(): BannerImage {
-        const randomIndex = Math.floor(Math.random() * bannerImages.length);
-        return bannerImages[randomIndex];
+        //const randomIndex = Math.floor(Math.random() * bannerImages.length);
+        //return bannerImages[randomIndex];
+        return bannerImages[1];
+    }
+
+
+    function BlurredBackgroundBanner({ img }: { img: BannerImage }) {
+        return (
+            <div style={{ position: "relative", width: "100%", height: "100%" }}>
+                {/* Blurred background */}
+                <img
+                    src={img.src}
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        objectPosition: img.objectPosition,
+                        filter: "blur(16px) brightness(0.7)",
+                        zIndex: 0,
+                        pointerEvents: "none",
+                        userSelect: "none"
+                    }}
+                    aria-hidden="true"
+                />
+                {/* Main image, always fully visible */}
+                <img
+                    src={img.src}
+                    style={{
+                        position: "relative",
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "contain",
+                        objectPosition: img.objectPosition,
+                        zIndex: 1
+                    }}
+                />
+            </div>
+        );
+    }
+
+    function FullWidthBackgroundBanner({ img }: { img: BannerImage }) {
+        return (
+            <img
+                src={img.src}
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    objectPosition: img.objectPosition
+                }}
+            />
+        );
     }
 
     let img: BannerImage = getRandomBannerImage();
 
     return (
-        <Banner 
-            style={{height: "20vw", maxHeight: "500px", minHeight: "100px"}}
-            media={<img src={img.src} />}
-            mediaStyle={{ objectPosition: img.objectPosition, objectFit: img.objectFit, width: "100%", height: "100%", }}>
+        <Banner
+            style={{ height: "68vw", maxHeight: "400px", minHeight: "100px", position: "relative", overflow: "hidden" }}
+            media={
+                img.displayMode === "contain"
+                    ? <BlurredBackgroundBanner img={img} />
+                    : <FullWidthBackgroundBanner img={img} />
+            }
+            mediaStyle={{ width: "100%", height: "100%" }}
+        >
         </Banner>
     )
 }
--- content/banner.images.tsx
+++ content/banner.images.tsx
@@ -1,8 +1,15 @@
+
 export const bannerImages: BannerImage[] = [
     {
         src: "/images/indoor-track/losi-nascar-lineup.jpg",
         objectPosition: "center 80%",
-        objectFit: "cover"
+        objectFit: "cover",
+        displayMode: "fill"
+    }, {
+        src: "/images/indoor-track/february-freeze-2026.PNG",
+        objectPosition: "center 30%",
+        objectFit: "contain",
+        displayMode: "contain",
     }
 ]
 
@@ -10,4 +17,5 @@ export interface BannerImage {
     src: string;
     objectPosition: string;
     objectFit?: string | any;
+    displayMode?: "fill" | "contain";
 }
