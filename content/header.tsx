764bfe87d4b3033337effc7ddcd61cfb4fb6b894
Zack Jones <justzackj@gmail.com>
2026-01-24 19:51:58 -0700

    Use PNGs instead of SVGs
    Has weird artifacts when SVG vs PNG

--- content/header.tsx
+++ content/header.tsx
@@ -2,14 +2,14 @@ export const header = {
     // logo can be a single object or an array of objects
     logo: [
         {
-            src: "/images/logos/outdoor.gjrsr.cropped.svg",
+            src: "/images/logos/outdoor.gjrsr.png",
             alt: "Outdoor Dirt Oval Logo",
-            width: 150,
+            width: 175,
             height: 50,
         }, {
-            src: "/images/logos/indoor.gjrsr.cropped.svg",
+            src: "/images/logos/indoor.gjrsr.png",
             alt: "Indoor Carpet Oval Logo",
-            width: 150,
+            width: 175,
             height: 50,
         }
     ],
