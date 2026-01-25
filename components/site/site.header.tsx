2a40c0c177b12a7954beca8d1c95659042e28c44
Zack Jones <justzackj@gmail.com>
2026-01-24 19:30:56 -0700

    add both logos to header

--- components/site/site.header.tsx
+++ components/site/site.header.tsx
@@ -5,10 +5,16 @@ import { Row } from '@/components/ui/ui'
 import { header } from '@/content/content'
 
 export default function SiteHeader() {
+    let logos = Array.isArray(header.logo) ? header.logo : [header.logo];
+
+
+    function renderLogo(logo: any, index: number) {
+        return (<Image key={index} src={logo.src} alt={logo.alt} width={logo.width} height={logo.height}/>);
+    }
+
     return (
         <Row fullWidth shadowBottom className="p-4 z-50" align='center' justify='center' style={{ backgroundColor: "#ffffff" }}>
-            <Image src={header.logo.src} alt={header.logo.alt} 
-                width={header.logo.width} height={header.logo.height}/>
+            {logos.map(renderLogo)}
         </Row>
     )
 }
--- content/header.tsx
+++ content/header.tsx
@@ -1,11 +1,17 @@
 export const header = {
-    logo: {
-        src: "/images/logos/gjrsr-logo-white-bg-transparent.png",
-        alt: "GJ Riverside Raceway Logo",
-        width: 150,
-        height: 50,
-    },
-    items: [
-        
-    ]
+    // logo can be a single object or an array of objects
+    logo: [
+        {
+            src: "/images/logos/outdoor.gjrsr.cropped.svg",
+            alt: "Outdoor Dirt Oval Logo",
+            width: 150,
+            height: 50,
+        }, {
+            src: "/images/logos/indoor.gjrsr.cropped.svg",
+            alt: "Indoor Carpet Oval Logo",
+            width: 150,
+            height: 50,
+        }
+    ],
+    items: []
 }
