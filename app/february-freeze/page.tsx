8e8ad031e619b9fad7a2462a7f4afcff0c660f72
Zack Jones <justzackj@gmail.com>
2026-02-08 15:40:22 -0700

    add redirect if its a mobile device

--- app/february-freeze/page.tsx
+++ app/february-freeze/page.tsx
@@ -1,10 +1,27 @@
-import './style.css';
+"use client";
+
+import { useEffect, useState } from 'react';
+
 export default function FebruaryFreezePDF() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      const ua = navigator.userAgent;
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+    };
+    if (checkMobile()) {
+      setIsMobile(true);
+      window.location.href = "/results/February Freeze 2.7.2026 Results.pdf";
+    }
+  }, []);
+
+  if (isMobile) {
+    return <div style={{textAlign: 'center', marginTop: 40}}>Opening PDF...</div>;
+  }
+
   return (
-    <div style={{ height: '1000px', width: '100vw' }}>
-      <div className="mobile-zoom-tip">
-        If the PDF appears too small or large, pinch to zoom in or out.
-      </div>
+    <div style={{ height: '100%', width: '100vw' }}>
       <iframe
         src="/results/February Freeze 2.7.2026 Results.pdf"
         width="100%"
