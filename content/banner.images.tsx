bf125f5be556508fb48439c8dc6005cfeee2d129
Zack Jones <justzackj@gmail.com>
2026-03-22 08:55:09 -0600

    add new banner images

--- content/banner.images.tsx
+++ content/banner.images.tsx
@@ -1,14 +1,31 @@
 
 export const bannerImages: BannerImage[] = [
     {
-        src: "/images/fliers/february-freeze-2026.PNG",
-        objectPosition: "center 10%",
-        objectFit: "contain",
-        link: "/february-freeze"
+        src: "/images/indoor-track/dave-john-hackfab.jpg",
+        objectPosition: "center 55%",
+        objectFit: "cover",
+        active: true
+    }, {
+        src: "/images/indoor-track/john-maddox-zack-hackfab.jpg",
+        objectPosition: "center 82%",
+        objectFit: "cover",
+        active: true
+    }, {
+        src: "/images/indoor-track/zack-eric-rob-nascar.jpg",
+        objectPosition: "center 20%",
+        objectFit: "cover",
+        active: true
     }, {
         src: "/images/indoor-track/losi-nascar-lineup.jpg",
         objectPosition: "center 80%",
         objectFit: "cover",
+        active: true
+    }, {
+        src: "/images/fliers/february-freeze-2026.PNG",
+        objectPosition: "center 10%",
+        objectFit: "contain",
+        link: "/february-freeze",
+        active: false
     }
 ]
 
