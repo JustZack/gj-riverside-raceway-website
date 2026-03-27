3f924ecbc688c37a6c4e256b46a0bc759b76a458
Zack Jones <justzackj@gmail.com>
2026-03-27 17:28:38 -0600

    Update route.ts

--- app/redirects/[slug]/route.ts
+++ app/redirects/[slug]/route.ts
@@ -3,9 +3,9 @@ import { redirect } from 'next/navigation';
 
 export async function GET(
   request: Request,
-  { params }: { params: { slug: string } }
+  { params }: { params: Promise<{ slug: string }> }
 ) {
-  const { slug } = params;
+  const { slug } = await params;
   const targetUrl = redirects[slug];
 
   // If no redirect found, redirect to home page
