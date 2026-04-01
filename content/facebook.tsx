887bf6857204b65a53caebc655a74d611544f5e1
Zack Jones <justzackj@gmail.com>
2026-04-01 10:08:45 -0600

    add checkMeContent for facebook

--- content/facebook.tsx
+++ content/facebook.tsx
@@ -2,4 +2,9 @@ export const facebook = {
     baseUrl: 'https://www.facebook.com/',
     groupUrl: `https://www.facebook.com/share/g/1bRsCDdtpa/`,
     groupName: 'Grand Valley RC Racing',
+    checkMeContent: ():  React.JSX.Element => (
+        <a href={facebook.groupUrl} target="_blank" rel="noopener noreferrer" className='text-[#1877F2]'>
+            Check facebook for more info!
+        </a>
+    )
 }
