b6517b65f18d9a7b93a13db0cca50c36779f59e5
Zack Jones <justzackj@gmail.com>
2026-04-01 09:26:44 -0600

    Move scraped event to model

--- lib/jobs/sync.livetimerc.content.job.tsx
+++ lib/jobs/sync.livetimerc.content.job.tsx
@@ -4,6 +4,7 @@ import { Prisma }  from "@prisma/client";
 import Logger from "@/lib/utils/logger";
 import { livetime } from "@/content/content";
 import { parse, HTMLElement } from 'node-html-parser';
+import { ScrapedLiveTimeEvent } from "@/lib/jobs/models/scraped.livetime.event";
 
 export default async function SyncLiveTimeContentJob() {
     const logger: Logger = new Logger('SyncLiveTimeContentJob');
@@ -77,66 +78,4 @@ export default async function SyncLiveTimeContentJob() {
     }
 
     await scrapeAndUpsertEvents();
-}
-
-class ScrapedLiveTimeEvent {
-    event_id: number;
-    name: string;
-    date: string;
-    entries: number;
-    drivers: number;
-    livetime_path: string;
-    laps: number;
-
-    constructor(row: HTMLElement) {
-        const cols = row.querySelectorAll('td');
-        const first = cols[0];
-
-        // The ID is in a url like this: /results/?p=view_event&id=488422
-        let href = '';
-        const a = first?.querySelector('a');
-        if (a) {
-            href = a.getAttribute('href') || '';
-        }
-        this.event_id = parseInt(href.split('&id=').pop() || '0', 10);
-        this.name = first?.text.trim() || '';
-
-        // the date is formatted like: <span class="hidden">2025-12-02 00:00:00</span>Dec 2, 2025
-        const dateCol = cols[1];
-        const dateSpan = dateCol?.querySelector('span');
-        this.date = dateSpan?.text.trim() || '';
-
-        // These values are just numeric in the table
-        this.entries = parseInt(cols[2]?.text.trim() || '0', 10);
-        this.drivers = parseInt(cols[3]?.text.trim() || '0', 10);
-
-        this.livetime_path = `${livetime.resultsPath}${this.event_id}`;
-        this.laps = 0;
-    }
-
-    toTrackEvent(): Prisma.TrackEventCreateInput {
-        return {
-            name: this.name,
-            start: new Date(this.date),
-            end: new Date(this.date),
-            // @ts-ignore: livetimeID is in the schema but not in the generated type
-            livetimeID: this.event_id,
-        }
-    }
-
-    toLiveTimeEvent(): Prisma.LiveTimeEventCreateInput {
-        return {
-            id: this.event_id,
-            name: this.name,
-            entries: this.entries,
-            drivers: this.drivers,
-            laps: this.laps,
-            startedAt: new Date(this.date),
-            trackEvent: undefined
-        }
-    }
-
-    toString(): string {
-        return `ScrapedLiveTimeEvent { event_id: ${this.event_id}, name: ${this.name}, date: ${this.date}, entries: ${this.entries}, drivers: ${this.drivers}, livetime_path: ${this.livetime_path}, laps: ${this.laps} }`;
-    }
 }
